0a9e2ba209022e828d6a1752cc9c7baf04a700fc
vladrbyte <61164697+vladrbyte@users.noreply.github.com>
2026-06-18 14:51:30 +0200

    Update next.config.ts with export and basePath
    Added output and basePath options to Next.js config.
    for pages.github

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',              // Включает экспорт в статический HTML
+  basePath: '/kanban_tnmt',      // Указывает базовый адрес вашего репозитория
+  images: {
+    unoptimized: true,           // Отключает серверную оптимизацию картинок (на GH Pages нет Node.js сервера)
+  },
 };
 
 export default nextConfig;
